0aa4f4b3ed411304c5a44fd7f7b41e9b61c995a7
xaquin <xaquin.perez1@udc.es>
2020-11-04 18:47:29 +0100

    Add basics operations to OrderDao and OrderLineDao

---
+++ PracticaMaD/Model/OrderDao/OrderDaoEntityFramework.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Es.Udc.DotNet.ModelUtil.Dao;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.OrderDao
+{
+    public class OrderDaoEntityFramework:
+        GenericDaoEntityFramework<Order_Table, Int64>, IOrderDao
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public OrderDaoEntityFramework()
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region IOrderLineDao Members. Specific Operations
+        public Order_Table FindById(long id)
+        {
+            Order_Table order = null;
+
+            DbSet<Order_Table> orders = Context.Set<Order_Table>();
+
+            var result =
+                (from o in orders
+                 where o.id == id
+                 select o);
+
+            order = result.FirstOrDefault();
+
+            if (order == null)
+                throw new InstanceNotFoundException(id,
+                    typeof(Order_Table).FullName);
+
+            return order;
+        }
+
+        #endregion IUserProfileDao Members
+    }
+}
