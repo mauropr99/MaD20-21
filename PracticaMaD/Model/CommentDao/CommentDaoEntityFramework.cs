df976b5706efdd861624095aad83efe47bfd7ba8
Mauro Paredes Romero <mauro.paredes@udc.es>
2020-11-04 18:31:15 +0100

    Add basic operations to ProductDao and CommentDao

---
+++ PracticaMaD/Model/CommentDao/CommentDaoEntityFramework.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Es.Udc.DotNet.ModelUtil.Dao;
+using Microsoft.EntityFrameworkCore;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
+{
+    class CommentDaoEntityFramework :
+        GenericDaoEntityFramework<Comment, Int64>, ICommentDao
+    {
+        List<Comment> FindByProductId(long productId)
+        {
+            #region Using Linq.
+
+            DbSet<Comment> comments = Context.Set<Comment>();
+
+            var result =
+                (from c in comments
+                 where c.productId == productId
+                 orderby c.commentDate
+                 select c).Skip(startIndex).Take(count).ToList();
+
+            return result;
+
+            #endregion Using Linq.
+        }
+    }
+}
---
+++ PracticaMaD/Model/CommentDao/ICommentDao.cs
@@ -0,0 +1,19 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using System;
+using System.Collections.Generic;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
+{
+    interface ICommentDao : IGenericDao<Comment, Int64>
+    {
+        /// <summary>
+        /// Returns a list of comments pertaining to a given product identifier.
+        /// </summary>
+        /// <param name="productId">the product identifier </param>
+        /// <param name="startIndex">the index of the first comment to return (starting in 0)</param>
+        /// <param name="count">the maximum number of commmets to return</param>
+        /// <returns>the list of comments</returns>
+        List<Comment> FindByProductId(long productId);
+    }
+}
