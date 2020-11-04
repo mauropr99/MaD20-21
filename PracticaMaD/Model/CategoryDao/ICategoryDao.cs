acc12bdedce4fecf7ece57e9f5df58b7597418fe
Santiago Barreiro Hermida <santiago.barreiro@udc.es>
2020-11-04 18:35:07 +0100

    Forgotten file added.

---
+++ PracticaMaD/Model/CategoryDao/ICategoryDao.cs
@@ -0,0 +1,11 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using System;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.CategoryDao
+{
+    public interface IUserDao : IGenericDao<Category, Int64>
+    {
+        Category FindByName(String name);
+    }
+}
