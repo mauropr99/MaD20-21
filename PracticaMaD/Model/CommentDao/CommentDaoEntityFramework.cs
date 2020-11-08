fed49a256f9a29d8ab72d6ff75275803b594fcbd
Mauro Paredes Romero <mauro.paredes@udc.es>
2020-11-08 19:03:50 +0100

    Finished Search Products and some changes on comment and product DAOs

--- PracticaMaD/Model/CommentDao/CommentDaoEntityFramework.cs
+++ PracticaMaD/Model/CommentDao/CommentDaoEntityFramework.cs
@@ -1,26 +1,23 @@
-﻿using System;
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Es.Udc.DotNet.ModelUtil.Dao;
-using Microsoft.EntityFrameworkCore;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
 {
-    class CommentDaoEntityFramework :
+    public class CommentDaoEntityFramework :
         GenericDaoEntityFramework<Comment, Int64>, ICommentDao
     {
-        List<Comment> FindByProductId(long productId)
+        public List<Comment> FindByProductId(long productId, int startIndex, int count)
         {
             #region Using Linq.
 
             DbSet<Comment> comments = Context.Set<Comment>();
 
-            var result =
+            List<Comment> result =
                 (from c in comments
                  where c.productId == productId
-                 orderby c.commentDate
                  select c).Skip(startIndex).Take(count).ToList();
 
             return result;
@@ -29,3 +26,4 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
         }
     }
 }
+
---
+++ PracticaMaD/Model/ProductService/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Es.Udc.DotNet.PracticaMaD.Model.ProductDao;
+using Ninject;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ProductService
+{
+    public class ProductService: IProductService
+    {
+        public ProductService()
+        {
+        }
+
+        [Inject]
+        public IProductDao ProductDao { private get; set; }
+
+        #region IProductService Members
+
+        public ProductBlock FindProductsByNameAndCategory(string productName, string categoryName, int startIndex, int count)
+        {
+            List<Product> products;
+
+            /*
+            * Find count+1 products to determine if there exist more products above
+            * the specified range.
+            */
+            if (categoryName == null)
+            {
+                products = ProductDao.FindByProductName(productName, startIndex, count + 1);
+
+            } 
+            else
+            {
+                products = ProductDao.FindByProductNameAndCategoryId(productName, categoryName, startIndex, count + 1);
+            }
+
+            bool existMoreProducts = (products.Count == count + 1);
+
+            if (existMoreProducts)
+            {
+                products.RemoveAt(count);
+            }
+
+            return new ProductBlock(products, existMoreProducts);
+
+        }
+
+        #endregion IProductService Members
+    }
+}
