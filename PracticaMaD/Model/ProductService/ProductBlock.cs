04bdd6644b46005537d919c394b65c75766cee42
Santiago Barreiro Hermida <santiago.barreiro@udc.es>
2020-11-09 20:34:50 +0100

    Removing repeated code.

--- PracticaMaD/Model/ProductService/ProductBlock.cs
+++ PracticaMaD/Model/ProductService/ProductBlock.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.ProductService
@@ -16,22 +15,3 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.ProductService
         }
     }
 }
-=======
-﻿using System.Collections.Generic;
-
-
-namespace Es.Udc.DotNet.PracticaMaD.Model.ProductService
-{
-    public class ProductBlock
-    {
-        public List<Product> Products { get; private set; }
-        public bool ExistMoreProducts { get; private set; }
-
-        public ProductBlock(List<Product> products, bool existMoreProducts)
-        {
-            this.Products = products;
-            this.ExistMoreProducts = existMoreProducts;
-        }
-    }
-}
->>>>>>> 979e3d2518bd9588b193c7bbec9949a7347d4b81
--- PracticaMaD/Model/ProductService/ProductService.cs
+++ PracticaMaD/Model/ProductService/ProductService.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Es.Udc.DotNet.PracticaMaD.Model.ProductDao;
 using Ninject;
 using Es.Udc.DotNet.ModelUtil.Dao;
@@ -63,69 +62,3 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.ProductService
     }
 
 }
-=======
-﻿using System.Collections.Generic;
-using Es.Udc.DotNet.PracticaMaD.Model.ProductDao;
-using Ninject;
-using Es.Udc.DotNet.ModelUtil.Dao;
-using Microsoft.AspNetCore.Authorization;
-
-namespace Es.Udc.DotNet.PracticaMaD.Model.ProductService
-{
-    public class ProductService: GenericDaoEntityFramework<Product, long>, IProductService
-    {
-        public ProductService()
-        {
-        }
-
-        [Inject]
-        public IProductDao ProductDao { private get; set; }
-
-        #region IProductService Members
-
-        public ProductBlock FindProductsByNameAndCategory(string productName, string categoryName, int startIndex, int count)
-        {
-            List<Product> products;
-
-            /*
-            * Find count+1 products to determine if there exist more products above
-            * the specified range.
-            */
-            if (categoryName == null)
-            {
-                products = ProductDao.FindByProductName(productName, startIndex, count + 1);
-
-            } 
-            else
-            {
-                products = ProductDao.FindByProductNameAndCategoryId(productName, categoryName, startIndex, count + 1);
-            }
-
-            bool existMoreProducts = (products.Count == count + 1);
-
-            if (existMoreProducts)
-            {
-                products.RemoveAt(count);
-            }
-
-            return new ProductBlock(products, existMoreProducts);
-
-        }
-
-        public void UpdateProduct(long productId, string productName,  int stock, decimal price)
-        {
-      
-                Product prod = ProductDao.FindById(productId);
-                prod.product_name = productName;
-                prod.stock = stock;
-                prod.price = price;
-                ProductDao.Update(prod);
-  
-        }
-       
-        #endregion IProductService Members
-
-    }
-
-}
->>>>>>> 979e3d2518bd9588b193c7bbec9949a7347d4b81
