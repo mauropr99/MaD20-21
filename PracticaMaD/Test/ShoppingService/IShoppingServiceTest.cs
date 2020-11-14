0cbd4a96d4f550177aa0a288f56db4c3c54e76cf
Santiago Barreiro Hermida <santiago.barreiro@udc.es>
2020-11-14 16:17:36 +0100

    Progress in test

---
+++ PracticaMaD/Test/ShoppingService/IShoppingServiceTest.cs
@@ -0,0 +1,88 @@
+﻿using Es.Udc.DotNet.MiniBank.Test;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ShoppingService.Tests
+{
+    /// <summary>
+    ///This is a test class for IAccountServiceTest and is intended
+    ///to contain all IAccountServiceTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class IShoppingServiceTest
+    {
+        private static IKernel kernel;
+        private static IShoppingService shoppingService;
+
+        // Variables used in several tests are initialized here
+        private const long userId = 123456;
+
+        private const long NON_EXISTENT_ACCOUNT_ID = -1;
+        private const long NON_EXISTENT_USER_ID = -1;
+
+        //Due to the limited precision of floating point numbers, the equality
+        //operator may provide unexpected results if two numbers are close to
+        //each other (e.g. 25 and 25.00000000001). In order to solve this
+        //issue, a small margin of error (delta) can be allowed.
+        private const double delta = 0.00001;
+
+        private TransactionScope transactionScope;
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+
+        //Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            kernel = TestManager.ConfigureNInjectKernel();
+            shoppingService = kernel.Get<IShoppingService>();
+        }
+
+        //Use ClassCleanup to run code after all tests in a class have run
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            TestManager.ClearNInjectKernel(kernel);
+        }
+
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            transactionScope = new TransactionScope();
+        }
+
+        //Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            transactionScope.Dispose();
+        }
+
+        #endregion Additional test 
+        
+    }
+}
---
+++ PracticaMaD/Test/TestManager.cs
@@ -0,0 +1,71 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model.OrderDao;
+using Es.Udc.DotNet.PracticaMaD.Model.OrderLineDao;
+using Es.Udc.DotNet.PracticaMaD.Model.ShoppingService;
+using Ninject;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace Es.Udc.DotNet.MiniBank.Test
+{
+    public class TestManager
+    {
+        /// <summary>
+        /// Configures and populates the Ninject kernel
+        /// </summary>
+        /// <returns>The NInject kernel</returns>
+        public static IKernel ConfigureNInjectKernel()
+        {
+            #region Option A : configuration via sourcecode
+
+            IKernel kernel = new StandardKernel();
+
+            kernel.Bind<IShoppingService>().To<ShoppingService>();
+
+            kernel.Bind<IOrderDao>().
+                To<OrderDaoEntityFramework>();
+
+            kernel.Bind<IOrderLineDao>().
+                To<OrderLineDaoEntityFramework>();
+
+            string connectionString =
+                ConfigurationManager.ConnectionStrings["PracticaMaDEntities"].ConnectionString;
+
+            kernel.Bind<DbContext>().
+                ToSelf().
+                InSingletonScope().
+                WithConstructorArgument("nameOrConnectionString", connectionString);
+
+            #endregion Option A : configuration via sourcecode
+
+            #region Option B: configuration via external XML configuration file
+
+            // The kernel should automatically load extensions at startup
+            //NinjectSettings settings = new NinjectSettings() { LoadExtensions = false };
+            //IKernel kernel = new StandardKernel(settings, new Ninject.Extensions.Xml.XmlExtensionModule());
+
+            //kernel.Load("Ninject_Config.xml");
+
+            #endregion Option B: configuration via external XML configuration file
+
+            return kernel;
+        }
+
+        /// <summary>
+        /// Configures the Ninject kernel from an external module file.
+        /// </summary>
+        /// <param name="moduleFilename">The module filename.</param>
+        /// <returns>The NInject kernel</returns>
+        public static IKernel ConfigureNInjectKernel(string moduleFilename)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Load(moduleFilename);
+
+            return kernel;
+        }
+
+        public static void ClearNInjectKernel(IKernel kernel)
+        {
+            kernel.Dispose();
+        }
+    }
+}
