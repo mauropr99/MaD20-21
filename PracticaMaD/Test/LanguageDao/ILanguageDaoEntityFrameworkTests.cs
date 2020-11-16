ab396aaf5e37638c628cd357e496bfa34267fe56
Santiago Barreiro Hermida <santiago.barreiro@udc.es>
2020-11-16 16:32:14 +0100

    FindByNameTest (LanguageDao) finished and succesfully executed.

--- PracticaMaD/Test/LanguageDao/ILanguageDaoEntityFrameworkTests.cs
+++ PracticaMaD/Test/LanguageDao/ILanguageDaoEntityFrameworkTests.cs
@@ -1,13 +1,9 @@
-﻿using Es.Udc.DotNet.ModelUtil.Dao;
-using Es.Udc.DotNet.ModelUtil.Exceptions;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity;
+using Ninject;
+using Es.Udc.DotNet.PracticaMaD.Test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Es.Udc.DotNet.PracticaMaD;
+using System.Transactions;
 
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.LanguageDao.Tests
@@ -15,6 +11,90 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.LanguageDao.Tests
     [TestClass()]
     public class ILanguageDaoEntityFrameworkTests
     {
-       
+        private static IKernel kernel;
+        private static ILanguageDao languageDao;
+
+        // Variables used in several tests are initialized here
+        private const long userId = 123456;
+        private const long NON_EXISTENT_USER_ID = -2;
+
+        private TransactionScope transactionScope;
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+
+        //Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            kernel = TestManager.ConfigureNInjectKernel();
+            languageDao = kernel.Get<ILanguageDao>();
+        }
+
+        //Use ClassCleanup to run code after all tests in a class have run
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            TestManager.ClearNInjectKernel(kernel);
+        }
+
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            transactionScope = new TransactionScope();
+        }
+
+        //Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            transactionScope.Dispose();
+        }
+
+        #endregion Additional test attributes
+
+        [TestMethod()]
+        public void FindByNameTest()
+        {
+            int numberOfLanguages = 11;
+
+            List<Language> createdLanguage = new List<Language>(numberOfLanguages);
+
+            /* Create 11 numberOfLanguages */
+            for (int i = 0; i < numberOfLanguages; i++)
+            {
+                Language language = new Language
+                {
+                    name = "language " + i.ToString(),
+                    country = "ES"
+                };
+                languageDao.Create(language);
+                createdLanguage.Add(language);
+            }
+
+            String expectedLanguageName = "language 8";
+            Language foundlanguage = languageDao.FindByName(expectedLanguageName);
+
+            Assert.AreEqual(foundlanguage.name, expectedLanguageName);
+
+        }
     }
 }
