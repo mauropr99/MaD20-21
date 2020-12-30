4e81592371e26b74cceedc8795a750f3b7295153
Santiago Barreiro Hermida <santiago.barreiro@udc.es>
2020-12-30 23:14:29 +0100

    New button added to the CreditCardView.

--- PracticaMaD/Web/Pages/CreditCardOperations/CreditCardView.aspx.cs
+++ PracticaMaD/Web/Pages/CreditCardOperations/CreditCardView.aspx.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using Es.Udc.DotNet.PracticaMaD.Model.UserService;
 using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
 using Es.Udc.DotNet.PracticaMaD.Web.HTTP.View.ApplicationObjects;
-
+using System.Web.UI.WebControls;
+using System.Globalization;
 
 namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.CreditCardOperations
 {
@@ -26,12 +27,21 @@ namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.CreditCardOperations
                 (UserSession)Context.Session[SessionManager.USER_SESSION_ATTRIBUTE];
 
             List<CreditCardDetails> creditCards = userService.FindCreditCardsByUserId(userSession.UserId);
-            
 
-            this.GridViewCreditCards.DataSource = creditCards;
+            UserDetails userDetails = userService.FindUserDetails(userSession.UserId);
 
-            this.GridViewCreditCards.DataBind();
+            //Cargamos la tarjeta por defecto (en caso de existir)
+            foreach (var creditCard in creditCards)
+            {
+                if (creditCard.CreditCardId == userDetails.DefaultCreditCardId)
+                {
+                    creditCard.IsDefaultCreditCard = true;
+                }
+            }
 
+            GridViewCreditCards.DataSource = creditCards;
+
+            GridViewCreditCards.DataBind();
 
 
             //Changing the date format...
@@ -53,19 +63,57 @@ namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.CreditCardOperations
 
             for (int i = 0; i < GridViewCreditCards.Rows.Count; i++)
             {
+                if(GridViewCreditCards.DataKeys[i].Values[1].ToString() == "True")
+                {
+                    GridViewCreditCards.Rows[i].Cells[4].Visible = false;
+                }
                 GridViewCreditCards.Rows[i].Cells[3].Text = creditCards[i].ExpirationDate.ToString(format);
             }
 
         }
 
-        protected void GridViewCreditCards_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         protected void btnAddNewCreditCard_Click(object sender, EventArgs e)
         {
             Response.Redirect(Response.ApplyAppPathModifier("~/Pages/CreditCardOperations/CreditCardAdd.aspx"));
+        }
+
+        protected void GridViewCreditCards_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "SetAsDefault")
+            {
+                
+                //1 Obtener contexto de inyección de dependencias
+
+                IIoCManager iocManager = (IIoCManager)Application["managerIoC"];
+
+                //2 Obtener el servicio
+
+                IUserService userService = iocManager.Resolve<IUserService>();
+
+                //Llamar a los casos de uso
+                UserSession userSession =
+                    (UserSession)Context.Session[SessionManager.USER_SESSION_ATTRIBUTE];
+                
+
+                int index = Convert.ToInt32(e.CommandArgument);
+
+                long creditCardId = long.Parse(GridViewCreditCards.DataKeys[index].Values[0].ToString());
+
+
+                userService.SetCreditCardAsDefault(userSession.UserId, creditCardId);
+
+                /*
+                 Volvemos a poner todos los botones visibles e invisibilizamos aquel que corresponda
+                 al índice de la tarjeta que hemos seleccionado como tarjeta por defecto.
+                 */
+                for (int i = 0; i < GridViewCreditCards.Rows.Count; i++)
+                    GridViewCreditCards.Rows[i].Cells[4].Visible = true;
+
+                GridViewCreditCards.Rows[index].Cells[4].Visible = false;
+
+            }
+
+
         }
     }
 }
